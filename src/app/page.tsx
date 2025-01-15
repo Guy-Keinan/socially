8f2ae051415c6c3c352b9b7e121e065dd6aad4ac
Guy Keinan <g.keinan91@gmail.com>
2025-01-15 23:11:15 +0200

    create navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
-import ModeToggle from '../components/ModeToggle';
+import ModeToggle from "../components/ModeToggle";
 
 export default function Home() {
   return (
-    <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <ModeToggle/>
+    <div className="m-4">
+      <h1>Home Page</h1>
     </div>
   );
 }
